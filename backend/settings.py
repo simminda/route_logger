d350ecc18ba89a3197585206fd5b60dc9e870eef
Simphiwe Ndaba <simtabz@gmail.com>
2025-04-08 19:21:37 +0200

    Use a wildcard for allowed origin

--- backend/settings.py
+++ backend/settings.py
@@ -161,7 +161,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite frontend
     "http://localhost:8000",  # Django Backend
-    "https://routelogger-620vkyk5r-simphiwe-ndabas-projects.vercel.app",
-    "https://routelogger-58bj3c0nr-simphiwe-ndabas-projects.vercel.app", 
     "https://route-logger.onrender.com",
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://routelogger-.*-simphiwe-ndabas-projects\.vercel\.app$",
 ]
