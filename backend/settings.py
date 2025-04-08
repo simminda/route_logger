e303b8c24d42bf9b91154825991f726fa9f3a25d
Simphiwe Ndaba <simtabz@gmail.com>
2025-04-08 19:31:04 +0200

    ensure django-cors-headers is added before the CommonMiddleware

--- backend/settings.py
+++ backend/settings.py
@@ -57,12 +57,12 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
@@ -162,8 +162,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite frontend
     "http://localhost:8000",  # Django Backend
     "https://route-logger.onrender.com",
+    "https://routelogger.vercel.app", 
 ]
 
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://routelogger-.*-simphiwe-ndabas-projects\.vercel\.app$",
-]
